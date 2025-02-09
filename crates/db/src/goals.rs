ecb08d14bc55232c083c0d09f163f360a54966b0
Samuel Wahome <samuelwahome.k5@gmail.com>
2025-02-09 22:42:42 +0300

    chore: add db CRUD functions

---
+++ crates/db/src/goals.rs
@@ -0,0 +1,93 @@
+use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
+
+#[derive(Serialize, Deserialize, Debug, sqlx::FromRow)]
+pub struct Goal {
+    pub id: i64,
+    pub name: String,
+    pub target_duration: i64,
+    pub frequency: String,
+    pub exclude_days: Option<String>,
+    pub progress: i64,
+    pub metadata: Option<String>,
+}
+
+impl Goal {
+    /// Insert a new goal
+    pub async fn insert(pool: &SqlitePool, goal: Goal) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "INSERT INTO goals (name, target_duration, frequency, exclude_days, progress, metadata)
+            VALUES (?, ?, ?, ?, ?, ?)",
+            goal.name,
+            goal.target_duration,
+            goal.frequency,
+            goal.exclude_days,
+            goal.progress,
+            goal.metadata
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    /// Update a goal progress by a specified amount
+    pub async fn update_progress(pool: &SqlitePool, goal_id: i64, progress_inc: i64) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "UPDATE goals SET progress = progress + ? WHERE id = ?",
+            progress_inc,
+            goal_id
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    /// Fetch all goals.
+    pub async fn fetch_all(pool: &SqlitePool) -> Result<Vec<Goal>, sqlx::Error> {
+        let rows = sqlx::query!(
+            r#"
+            SELECT id, name, target_duration, frequency, exclude_days, progress, metadata
+            FROM goals
+            "#
+        )
+        .fetch_all(pool)
+        .await?;
+
+        let goals: Vec<Goal> = rows.into_iter().map(|row| Goal {
+            id: row.id, // Unwrap the Option<i64>
+            name: row.name,
+            target_duration: row.target_duration,
+            frequency: row.frequency,
+            exclude_days: row.exclude_days,
+            progress: row.progress.unwrap_or_default(),
+            metadata: row.metadata,
+        }).collect();
+
+        Ok(goals)
+    }
+
+    /// Fetch a specific goal by its name.
+    pub async fn fetch_by_name(pool: &SqlitePool, goal_name: &str) -> Result<Option<Goal>, sqlx::Error> {
+        let row = sqlx::query!(
+            r#"
+            SELECT id, name, target_duration, frequency, exclude_days, progress, metadata
+            FROM goals WHERE name = ?
+            "#,
+            goal_name
+        )
+        .fetch_optional(pool)
+        .await?;
+
+        let goal = row.map(|row| Goal {
+            id: row.id,
+            name: row.name,
+            target_duration: row.target_duration,
+            frequency: row.frequency,
+            exclude_days: row.exclude_days,
+            progress: row.progress.unwrap_or_default(),
+            metadata: row.metadata,
+        });
+
+        Ok(goal)
+    }
+}
---
+++ crates/db/src/tags.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tag {
+    pub id: Option<i64>,
+    pub name: String,
+}
+
+impl Tag {
+    /// Insert a new tag
+    pub async fn insert(pool: &SqlitePool, tag: Tag) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "INSERT OR IGNORE INTO TAGS (name) VALUES (?)",
+            tag.name
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    /// Fetch all tags
+    pub async fn fetch_all(pool: &SqlitePool) -> Result<Vec<Tag>, sqlx::Error> {
+        let rows = sqlx::query_as!(
+            Tag,
+            "SELECT id, name FROM tags"
+        )
+        .fetch_all(pool)
+        .await?;
+        Ok(rows)
+    }
+
+    /// Fetch a tag by name
+    pub async fn fetch_by_name(pool: &SqlitePool, tag_name: &str) -> Result<Option<Tag>, sqlx::Error> {
+        sqlx::query_as!(
+            Tag,
+            "SELECT id, name FROM tags WHERE name = ?",
+            tag_name
+        )
+        .fetch_optional(pool)
+        .await
+    }
+
+    /// Associate a tag with an event via the `event_tags` table
+    pub async fn associate_tag_with_event(pool: &SqlitePool, event_id: i64, tag_id: i64) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "INSERT OR IGNORE INTO event_tags (event_id, tag_id) VALUES (?, ?)",
+            event_id,
+            tag_id
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    /// Fetch all tags associated with a specifiec event
+    pub async fn fetch_tags_for_event(pool: &SqlitePool, event_id: i64) -> Result<Vec<Tag>, sqlx::Error> {
+        let rows = sqlx::query_as!(
+            Tag,
+            "SELECT t.id, t.name
+            FROM tags t
+            JOIN event_tags et ON t.id = et.tag_id
+            WHERE et.event_id = ?",
+            event_id
+        )
+        .fetch_all(pool)
+        .await?;
+        Ok(rows)
+    }
+}
