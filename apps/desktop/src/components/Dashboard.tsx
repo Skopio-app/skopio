88f24d8573b61e1845ba907c1187300e4e7f65d8
Samuel Wahome <samuelwahome.k5@gmail.com>
2025-08-21 00:45:11 +0300

    chore: small styling fixes

--- apps/desktop/src/components/Dashboard.tsx
+++ apps/desktop/src/components/Dashboard.tsx
@@ -16,6 +16,7 @@ import {
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { builtinExtensionRegistry } from "../extensions/registry";
 import { LAST_ACTIVE_TAB } from "../utils/constants";
+import { Cog } from "lucide-react";
 
 const DashboardLayout = () => {
   const navigate = useNavigate();
@@ -34,12 +35,16 @@ const DashboardLayout = () => {
       >
         <div className="w-[64px] h-full" />
 
-        <div className="absolute left-[90px] top-[6px]">
-          <SidebarTrigger className="w-5 h-5 cursor-pointer" />
+        <div className="absolute left-[90px] top-[6px] flex flex-row space-x-3">
+          <SidebarTrigger className="w-5 h-5 cursor-pointer hover:bg-neutral-200" />
+          <Cog className="h-5 w-5 p-0.5 cursor-pointer hover:bg-neutral-200" />
         </div>
       </div>
 
-      <Sidebar collapsible="offcanvas" className="pt-[10px] bg-transparent">
+      <Sidebar
+        collapsible="offcanvas"
+        className="pt-[10px] border-border bg-neutral-50"
+      >
         <SidebarHeader />
         <SidebarContent>
           <SidebarGroup>
@@ -49,6 +54,10 @@ const DashboardLayout = () => {
                 {tabExtensions.map((ext) => (
                   <SidebarMenuItem key={ext.manifest.id}>
                     <SidebarMenuButton
+                      tooltip={{
+                        children: ext.manifest.description,
+                        hidden: true,
+                      }}
                       isActive={location.pathname === "/tab/" + ext.manifest.id}
                       onClick={() => {
                         localStorage.setItem(LAST_ACTIVE_TAB, ext.manifest.id);
