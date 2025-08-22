ea449a277b14ca8a31460b476dcfe3ea27c4b50a
Samuel Wahome <samuelwahome.k5@gmail.com>
2025-08-22 23:29:04 +0300

    chore: add custom context menu

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,12 +1,39 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from "@skopio/ui";
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
 
 function App() {
   return (
-    <div>
+    <>
       <Toaster richColors />
-      <Outlet />
-    </div>
+      <ContextMenu>
+        <ContextMenuTrigger className="block min-h-screen w-full">
+          <Outlet />
+        </ContextMenuTrigger>
+        <ContextMenuContent className="w-52">
+          <ContextMenuItem inset>
+            Back
+            <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem inset disabled>
+            Forward
+            <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem inset>
+            Reload
+            <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuSeparator />
+        </ContextMenuContent>
+      </ContextMenu>
+    </>
   );
 }
 
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -23,3 +23,4 @@ export * from "./components/toggle";
 export * from "./components/toggle-group";
 export * from "./components/popover";
 export * from "./components/slider";
+export * from "./components/context-menu";
