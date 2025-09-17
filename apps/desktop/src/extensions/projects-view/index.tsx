5012170a21532619e80cece4b6f42145619bbd55
Samuel Wahome <samuelwahome.k5@gmail.com>
2025-09-18 01:16:54 +0300

    fix: adjust search icon position

--- apps/desktop/src/extensions/projects-view/index.tsx
+++ apps/desktop/src/extensions/projects-view/index.tsx
@@ -82,7 +82,7 @@ const ProjectsView = () => {
   return (
     <div className="flex flex-col h-full mx-4 space-y-4">
       <div className="relative w-full pt-2">
-        <SearchIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 text-muted-foreground size-4" />
+        <SearchIcon className="absolute left-3.5 top-6.5 -translate-y-1/2 text-muted-foreground size-4" />
         <Input
           placeholder="Search projects..."
           className="pl-10 max-w-md"
