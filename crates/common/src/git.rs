61450d04ba0686b82cd4c79c7d4f5ef6d4b7a6a1
Samuel Wahome <samuelwahome.k5@gmail.com>
2026-06-16 22:45:41 +0300

    chore: update Rust deps

--- crates/common/src/git.rs
+++ crates/common/src/git.rs
@@ -5,7 +5,7 @@ use git2::Repository;
 pub fn find_git_branch<T: AsRef<Path>>(from_path: T) -> Option<String> {
     if let Ok(repo) = Repository::discover(from_path)
         && let Ok(head) = repo.head()
-        && let Some(branch) = head.shorthand()
+        && let Ok(branch) = head.shorthand()
     {
         return Some(branch.to_string());
     }
