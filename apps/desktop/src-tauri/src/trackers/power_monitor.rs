67aa3f176410e7805afe5e9bd25104625796d971
Samuel Wahome <samuelwahome.k5@gmail.com>
2026-06-27 16:06:04 +0300

    chore: refactor autoreleasepool usage in power monitor

--- apps/desktop/src-tauri/src/trackers/power_monitor.rs
+++ apps/desktop/src-tauri/src/trackers/power_monitor.rs
@@ -2,8 +2,12 @@
 
 use block2::StackBlock;
 use chrono::{DateTime, Utc};
-use objc2::{class, msg_send, rc::Retained, runtime::AnyObject};
-use objc2_foundation::{NSAutoreleasePool, NSNotification, NSString};
+use objc2::{
+    class, msg_send,
+    rc::{Retained, autoreleasepool},
+    runtime::AnyObject,
+};
+use objc2_foundation::{NSNotification, NSString};
 use tokio::sync::broadcast;
 use tracing::error;
 
@@ -31,11 +35,20 @@ impl PowerMonitor {
     pub fn start(&self) {
         let tx = self.tx.clone();
 
-        std::thread::spawn(move || {
-            if let Err(err) = start_nsworkspace_power_observers(tx) {
-                error!("Failed to start power monitor: {err}");
+        match std::thread::Builder::new()
+            .name("skopio-power-monitor".into())
+            .spawn(move || {
+                if let Err(error) = run_power_monitor(tx) {
+                    error!("Failed to start power monitor: {error}");
+                }
+            }) {
+            Ok(_handle) => {
+                // Dropping JoinHandle detaches the thread, which is fine here
             }
-        });
+            Err(error) => {
+                error!("Failed to spawn monitor thread: {error}");
+            }
+        }
     }
 }
 
@@ -45,58 +58,73 @@ impl Default for PowerMonitor {
     }
 }
 
-fn start_nsworkspace_power_observers(tx: broadcast::Sender<PowerEvent>) -> Result<(), String> {
-    unsafe {
-        let _pool = NSAutoreleasePool::new();
-
-        let workspace: *mut AnyObject = msg_send![class!(NSWorkspace), sharedWorkspace];
-        if workspace.is_null() {
-            return Err("NSWorkspace.sharedWorkspace returned null".into());
-        }
-
-        let center: *mut AnyObject = msg_send![workspace, notificationCenter];
-        if center.is_null() {
-            return Err("NSWorkspace notificationCenter returned null".into());
-        }
-
-        let sleep_tx = tx.clone();
-        let sleep_block = StackBlock::new(move |_notification: *mut NSNotification| {
-            let _ = sleep_tx.send(PowerEvent::WillSleep { at: Utc::now() });
-        })
-        .copy();
-
-        let wake_tx = tx;
-        let wake_block = StackBlock::new(move |_notification: *mut NSNotification| {
-            let _ = wake_tx.send(PowerEvent::DidWake { at: Utc::now() });
-        })
-        .copy();
-
-        let will_sleep_name: Retained<NSString> =
-            NSString::from_str("NSWorkspaceWillSleepNotification");
-        let did_wake_name: Retained<NSString> =
-            NSString::from_str("NSWorkspaceDidWakeNotification");
-
-        let _sleep_observer: *mut AnyObject = msg_send![
-            center,
-            addObserverForName: &*will_sleep_name,
-            object: std::ptr::null_mut::<AnyObject>(),
-            queue: std::ptr::null_mut::<AnyObject>(),
-            usingBlock: &*sleep_block
-        ];
-
-        let _wake_observer: *mut AnyObject = msg_send![
-            center,
-            addObserverForName: &*did_wake_name,
-            object: std::ptr::null_mut::<AnyObject>(),
-            queue: std::ptr::null_mut::<AnyObject>(),
-            usingBlock: &*wake_block
-        ];
-
+fn run_power_monitor(tx: broadcast::Sender<PowerEvent>) -> Result<(), String> {
+    let run_loop = autoreleasepool(|_| unsafe {
+        register_power_observers(tx)?;
         let run_loop: *mut AnyObject = msg_send![class!(NSRunLoop), currentRunLoop];
-        let distant_future: *mut AnyObject = msg_send![class!(NSDate), distantFuture];
 
-        loop {
-            let _: () = msg_send![run_loop, runUntilDate: distant_future];
+        if run_loop.is_null() {
+            return Err("NSRunLoop.currentLoop returned null".into());
         }
+
+        Ok::<*mut AnyObject, String>(run_loop)
+    })?;
+
+    loop {
+        autoreleasepool(|_| unsafe {
+            let until: *mut AnyObject =
+                msg_send![class!(NSDate), dateWithTimeIntervalSinceNow: 60.0f64];
+            let _: () = msg_send![run_loop, runUntilDate: until];
+        })
     }
+}
+
+unsafe fn register_power_observers(tx: broadcast::Sender<PowerEvent>) -> Result<(), String> {
+    let workspace: *mut AnyObject = msg_send![class!(NSWorkspace), sharedWorkspace];
+    if workspace.is_null() {
+        return Err("NSWorkspace.sharedWorkspace returned null".into());
+    }
+
+    let center: *mut AnyObject = msg_send![workspace, notificationCenter];
+    if center.is_null() {
+        return Err("NSWorkspace notificationCenter returned null".into());
+    }
+
+    let sleep_tx = tx.clone();
+    let sleep_block = StackBlock::new(move |_notification: *mut NSNotification| {
+        let _ = sleep_tx.send(PowerEvent::WillSleep { at: Utc::now() });
+    })
+    .copy();
+
+    let wake_tx = tx;
+    let wake_block = StackBlock::new(move |_notification: *mut NSNotification| {
+        let _ = wake_tx.send(PowerEvent::DidWake { at: Utc::now() });
+    })
+    .copy();
+
+    let will_sleep_name: Retained<NSString> =
+        NSString::from_str("NSWorkspaceWillSleepNotification");
+    let did_wake_name: Retained<NSString> = NSString::from_str("NSWorkspaceDidWakeNotification");
+
+    let sleep_observer: *mut AnyObject = msg_send![
+        center,
+        addObserverForName: &*will_sleep_name,
+        object: std::ptr::null_mut::<AnyObject>(),
+        queue: std::ptr::null_mut::<AnyObject>(),
+        usingBlock: &*sleep_block
+    ];
+
+    let wake_observer: *mut AnyObject = msg_send![
+        center,
+        addObserverForName: &*did_wake_name,
+        object: std::ptr::null_mut::<AnyObject>(),
+        queue: std::ptr::null_mut::<AnyObject>(),
+        usingBlock: &*wake_block
+    ];
+
+    if sleep_observer.is_null() || wake_observer.is_null() {
+        return Err("Failed to register NSWorkspace power observers".into());
+    }
+
+    Ok(())
 }
